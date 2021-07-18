720f1a9d98d68b9bceca49bc2b762cac1f76a5f0
David Moftakhar <davidmoftakhar@gmail.com>
2021-07-18 14:26:57 +0100

    Toggle add task.

--- to-do-app/src/App.vue
+++ to-do-app/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
-      <Header title="Task Tracker"/>
-      <AddTask @addTask="addTask"/>
+      <Header @toggle-add-task="toggleAddTask" title="Task Tracker"/>
+      <div> 
+        <AddTask v-if="showAddTask" @addTask="addTask"/>
+      </div>
       <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
@@ -21,11 +23,14 @@ export default {
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false,
     }
   },
   methods: {
-
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
     addTask(task) {
       this.tasks = [...this.tasks, task]
     },
--- to-do-app/src/components/Button.vue
+++ to-do-app/src/components/Button.vue
@@ -11,7 +11,7 @@ export default {
     },
     methods: {
         onClick(){
-            console.log('test')
+            this.$emit('toggle-add-task')
         }
     }    
 }
